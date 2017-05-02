74116aff17ba7506d3fadfca73f57c5809a3c6f0
Ada Lovecraft <ada@codevinsky.com>
2017-05-02 10:16:29 -0500

    proper cross browser element iteration

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,8 @@ console.log('blerrrrrg')
 
 document.addEventListener("DOMContentLoaded", function() {
   const pres = document.getElementsByTagName('pre')
-  for(var pre of pres) {
+  for(let i = 0; i < pres.length; i++) {
+    const pre = pres[i]
     pre.classList.add('line-numbers')
   }
   Prism.highlightAll(true)
